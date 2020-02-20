1441890f513380ca4ce983dac8724be8f0ab73ad
soap1069 <soap1069@gmail.com>
2020-02-20 15:08:11 +0100

    Added mobile hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello! Welcome to Sophia's portfolio site")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
